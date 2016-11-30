c107e554c710d442a83885f96ca45ec3439d207a
kdesput <dowlander@gmail.com>
2016-11-30 18:53:42 +0100

    Added UI for Sudoku

--- SI2/SudokuWindow.xaml.cs
+++ SI2/SudokuWindow.xaml.cs
@@ -23,5 +23,46 @@ namespace SI2
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Let's solve that
+            int method; //BT or FC
+            if (radioButton.IsChecked == true) method = 0; //BT
+            else method = 1;
+            int N = comboBoxN.SelectedIndex + 1;
+            if (method == 0) //BT
+            {
+
+            }
+            else //FC
+            {
+
+            }
+        }
+
+        private void comboBoxN_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            comboBoxM.Items.Clear(); //cleaning results comboBox
+
+            //int j = 0;
+            for (int j = 0; j< Math.Pow(comboBoxN.SelectedIndex + 2, 4) - 1; j++)
+            {
+                Label newLabel = new Label();
+                newLabel.Content = j+1;
+                comboBoxM.Items.Add(newLabel);
+            }
+            comboBoxM.SelectedIndex = 0;
+        }
+
+        private void comboBoxM_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO change displayed sudoku
+        }
+
+        private void comboBoxHeuristic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
